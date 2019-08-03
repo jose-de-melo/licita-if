094be58936b4b1f67c3a70a20404c9e3b232fc56
José de Melo <josedemelo.if@gmail.com>
2019-08-03 19:40:53 -0300

    [UP] Finalizando a interface

--- src/chat/chatList.jsx
+++ src/chat/chatList.jsx
@@ -2,8 +2,13 @@ import React, { Component } from 'react'
 import {FaRobot, FaUser} from 'react-icons/fa'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import Emoji from 'react-emoji-render';
 
 import { getMessages } from './chatActions'
+
+const emojis = {
+    "wiking": '&#128523;'
+}
 
 class ChatList extends Component {
 
@@ -17,6 +22,16 @@ class ChatList extends Component {
         this.props.getMessages()
     }
 
+    renderLink(message){
+        const messageArray = message.split(';')
+        return(
+            <a className="link-chat" href={messageArray[1]} target="_blank">
+                {messageArray[2]}
+            </a>
+        )
+
+    }
+
     renderMessages() {
         const list = this.props.messages || []
         if(list.length > 0){
@@ -24,8 +39,12 @@ class ChatList extends Component {
                 <li key={index} className={`message ${item.base} appeared`} >
                     <div className="avatar">{(item.base == 'sent') ? '' : <FaRobot /> }</div>
                     <div className="text_wrapper">
-                        <div className="text">{item.message}</div>
-                        {(item.link) ? '' : ''}
+                        {
+                            (item.message.includes('<link>')) 
+                            ? this.renderLink(item.message) 
+                            : 
+                            <div className="text">{item.message}</div>
+                        }
                     </div>
                 </li>
             ))
--- src/links/about/about.jsx
+++ src/links/about/about.jsx
@@ -9,7 +9,7 @@ class About extends React.Component{
                 <div className="jumbotron w-75 mx-auto text-justify" style={{fontSize:"25px"}}> 
                     O LicitaIF foi desenvolvido para ser o trabalho final do curso 'Chatbots: da teoria ao deploy, com IBM Watson' do projeto de extensão 'Laboratório de Redes de Conhecimento', do Instituto Federal do Sudeste de Minas Gerais, <i>campus</i> Barbacena.
                     <p style={{marginTop:"3%"}}>
-                        A ideia era construir um site para ajudar os servidores do Instituto com Licitações. Tudo o que é comprado ou contratado pelo IF, passa por uma licitação ou dispensa da mesma. Para isso,
+                        O objetivo era construir um site simples, com ótima usabilidade e de fácil compreensão para ajudar os servidores do Instituto com Licitações. Tudo o que é comprado ou contratado pelo IF, passa por uma licitação ou dispensa da mesma. Para isso,
                         todo um processo admnistrativo é realizado. Porém, muitos dos servidores ainda têm dúvidas sobre o assunto. Para auxiliar com isso, o LicitaIF foi construído, afim de fornecer informações e
                         modelos de documentação utilizados no IF, manuais e informações sobre o Setor de Licitações do Instituto.
                     </p>                
